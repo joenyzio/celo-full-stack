7286f2b62fe1662d6ecc9209cc41b4c66f5a84f9
Joe Nyzio <jdnyzio@gmail.com>
2021-10-11 22:33:04 -0700

    deployed to celo

---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-typechain");
+require("@nomiclabs/hardhat-web3");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+task("balance", "Prints an account's balance")
+.addParam("account", "The account's address")
+.setAction(async taskArgs => {
+  const account = web3.utils.toChecksumAddress(taskArgs.account);
+  const balance = await web3.eth.getBalance(account);
+
+  console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 44787,
+    },
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      // accounts: [process.env.a2key]
+      accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
+    },
+    // rinkeby: {
+    //   url: "https://rinkeby.infura.io/v3/projectid",
+    //   accounts: [process.env.a2key]
+    // }
+  },
+  solidity: "0.8.3",
+};
+
